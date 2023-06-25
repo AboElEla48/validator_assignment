0ed1613538e723149a35232d8914523e17ff69aa
Aboelela <eng.a.aboelela@gmail.com>
2023-06-25 13:19:46 +0300

    Add input field to count text length

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+import React, {Component} from 'react';
+import ValidationComponent from './Validation/ValidationComponent.js';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+    textLength: 0
+  };
+
+  textChangeLengthHandler = (event) => {
+    this.setState( {textLength: event.target.value.length});
+
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <input
+          onChange = {this.textChangeLengthHandler}
+          placeHolder='Measure Text Length' />
+
+        <p>{this.state.textLength}</p>
+
+      </div>
+    );
+  }
 }
 
 export default App;
